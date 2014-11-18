cfba8dfb62ed6af1c5a64550210035fef13650c6
rihongo <rihongo@daou.co.kr>
2014-11-18 20:13:05 +0900

    start date 타입 수정

--- app/public/js/views/waiting.js
+++ app/public/js/views/waiting.js
@@ -10,19 +10,19 @@ $(document).ready(function() {
 	    var ii = this.getMinutes().toString();
 	    var ss = this.getSeconds().toString();
 
-	    return yyyy + (mm[1] ? mm : '0'+mm[0]) + (dd[1] ? dd : '0'+dd[0]) + 
-	    (hh[1] ? hh : '0'+hh[0]) + (ii[1] ? ii : '0'+ii[0]) + (ss[1] ? ss : '0'+ss[0]);
-	}	
+	    return yyyy + '-' + (mm[1] ? mm : '0'+mm[0]) + '-' + (dd[1] ? dd : '0'+dd[0]) + ' ' +
+	    (hh[1] ? hh : '0'+hh[0]) + ':' + (ii[1] ? ii : '0'+ii[0]) + ':' + (ss[1] ? ss : '0'+ss[0]);
+	}
 	
 	var startTime =  $("#startDate").val();
 	var clock = $('.clock').FlipClock({
 		clockFace: 'TwelveHourClock'
 	    
 	});
-	
+	var now= new Date();
+
 	setInterval(function(){
-		var now= new Date().yyyymmdd();
-	
+     		console.log(now.yyyymmdd());
 		if( now >= startTime ){
 			window.location.href = '/register';
 		}
